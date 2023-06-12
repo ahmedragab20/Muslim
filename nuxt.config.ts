da3e8c4a4c7749d30bdefacf85996ce93c719c5a
ahmedragab <ahmedragab20901@gmail.com>
2023-06-12 18:13:41 +0300

    👀 Give it a try

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   css: ['@/assets/css/core.css', '@/assets/css/main.css'],
   modules: ['@pinia/nuxt', '@nuxtjs/google-fonts', '@nuxthq/ui', '@nuxtjs/i18n'],
   i18n: {
-    vueI18n: './i18n.config.ts',
+    vueI18n: 'i18n.config',
   },
   pinia: {
     autoImports: ['defineStore', ['defineStore', 'definePiniaStore']],
