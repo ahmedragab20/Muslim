bfe8bc89dd600009bae9dafd556bb744d258b85d
ahmedragab <ahmedragab20901@gmail.com>
2023-06-13 21:41:14 +0300

    🙂 Declare the project road map

--- pages/quran/index.vue
+++ pages/quran/index.vue
@@ -4,6 +4,21 @@
       <UIcon name="i-heroicons-cog" class="w-20 h-20 text-primary-500 mx-auto" />
     </div>
     <div class="text-gray-800 dark:text-gray-400 font-mono">The Quran page</div>
+
+    <!-- TODOs:: -->
+    <!-- 1. search for a specific verse -->
+    <!-- 2. select a specific surah -->
+    <!-- 3. select a specific page -->
+    <!-- 4. select a specific juz -->
+    <!-- 5. read quraan only in english -->
+    <!-- 6. read quraan only in arabic -->
+    <!-- 7. read quraan in english and arabic -->
+    <!-- 9. add tafseer in arabic and english -->
+    <!-- 10. highlight a verse -->
+    <!-- 11. add a note to a verse -->
+    <!-- 12. add a bookmark to pages -->
+    <!-- 13. play quraan with list of reciters -->
+    <!-- 14. customize the size of the text -->
   </div>
 </template>
 
