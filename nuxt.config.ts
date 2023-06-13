289224696010c8881f1ca752ebb184678a46a848
ahmedragab <ahmedragab20901@gmail.com>
2023-06-13 06:58:10 +0300

    🏗️ Fix the i18n locales

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,23 @@ export default defineNuxtConfig({
   css: ['@/assets/css/core.css', '@/assets/css/main.css'],
   modules: ['@pinia/nuxt', '@nuxtjs/google-fonts', '@nuxthq/ui', '@nuxtjs/i18n'],
   i18n: {
-    vueI18n: 'i18n.config',
+    detectBrowserLanguage: {
+      useCookie: false,
+      fallbackLocale: 'en',
+    },
+    strategy: 'no_prefix',
+    locales: [
+      {
+        code: 'en',
+        file: 'en.json',
+      },
+      {
+        code: 'ar',
+        file: 'ar.json',
+      },
+    ],
+    langDir: 'localizations',
+    defaultLocale: 'en',
   },
   pinia: {
     autoImports: ['defineStore', ['defineStore', 'definePiniaStore']],
